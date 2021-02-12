c949a3284bba0613161cbbd2d93abaebf2c9158a
Kseniia <57846803+KseniiaO@users.noreply.github.com>
2021-02-13 00:51:13 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.282.b08-1.el7_9.x86_64/jre"
+export HADOOP_HOME=/usr/local/hadoop/current
+export HADOOP_HEAPSIZE_MAX=512
+export HADOOP_OS_TYPE=${HADOOP_OS_TYPE:-$(uname -s)}
